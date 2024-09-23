99c71a7099d1a734d417dcb8c88f3c6875b07a35
jroman <jromang@eservicios.indra.es>
2024-09-23 08:21:55 +0200

    test oled testing text

---
+++ expand.py
@@ -0,0 +1,27 @@
+import rocket_animation
+def duplicate_frame(frame_data, width=128, height=32):
+    expanded_frame = []
+    
+    # Iterar sobre cada fila de bytes (cada fila contiene width/8 bytes)
+    for row in range(height):
+        start = row * (width // 8)
+        end = start + (width // 8)
+        
+        # Obtener una fila completa del frame original
+        row_data = frame_data[start:end]
+        
+        # Duplicar la fila y añadirla dos veces al frame expandido
+        expanded_frame.extend(row_data)  # Primera vez
+        expanded_frame.extend(row_data)  # Segunda vez (duplicación)
+    
+    return expanded_frame
+def expand_frames(frames):
+    expanded_frames = []
+    for frame in frames:
+        expanded_frames.append(duplicate_frame(frame))
+    return expanded_frames
+
+expanded_python_frames = expand_frames(rocket_animation.frames)
+
+with open('expanded_rocket_animation.py','w') as era:
+    era.write(expanded_python_frames)
