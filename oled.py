6d6309793b245006189ef08e1efea173406f86b8
JaimeFromRasp <jaimeromangil@gmail.com>
2024-12-15 20:48:37 +0000

    changes

--- oled.py
+++ oled.py
@@ -21,7 +21,7 @@ i2c = busio.I2C(3, 2)  # (SCL, SDA)
 
 # Create the OLED display
 oled = SSD1306_I2C(128, 64, i2c)
-oled.contrast(5)
+oled.contrast(1)
 
 # Record the start time
 start_time = time.time()
